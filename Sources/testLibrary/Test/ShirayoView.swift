6423063e2b4092d37e98b32017b9b1b451a1ee11
Shirayo <iphoje@gmail.com>
2024-07-23 22:01:58 +0300

    added extention to make view neuromorphic

--- Sources/testLibrary/Test/ShirayoView.swift
+++ Sources/testLibrary/Test/ShirayoView.swift
@@ -8,14 +8,58 @@
 import Foundation
 import SwiftUI
 
-public struct ShirayoView: View {
+public struct ShirayoView: ViewModifier {
+    private var tint: Color
+//    private var lightShadow: Color
+//    private var darkShadow: Color
+    private var size: CGFloat = 40
+
+    public init(tint: Color, lightShadow: Color = .black, darkShadow: Color = .black) {
+        self.tint = tint
+//        self.lightShadow = lightShadow
+//        self.darkShadow = darkShadow
+    }
     
-    @State private var test: Int = 5
-    
-    public init() { }
-    
-    public var body: some View {
-        Text("Shirayo \(test)")
-            .foregroundStyle(.red)
+    public func body(content: Content) -> some View {
+        ZStack {
+            content
+                .frame(width: size, height: size)
+                .offset(x: size * 0.04, y: size * 0.04)
+                .blur(radius: size * 0.07)
+                .brightness(-0.2)
+            
+            content
+                .frame(width: size, height: size)
+                .offset(x: -size * 0.04, y: -size * 0.04)
+                .blur(radius: size * 0.07)
+                .brightness(0.1)
+            
+            content
+                .frame(width: size, height: size)
+        }
+    }
+}
+var color = Color.red
+var gray = Color(hex: "#313136")
+
+extension View {
+    func makeNeuromorphic(tint: Color) -> some View {
+        modifier(ShirayoView(tint: tint))
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        var cleanHexCode = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        cleanHexCode = cleanHexCode.replacingOccurrences(of: "#", with: "")
+        print(cleanHexCode)
+        var rgb: UInt64 = 0
+        
+        Scanner(string: cleanHexCode).scanHexInt64(&rgb)
+        
+        let redValue = Double((rgb >> 16) & 0xFF) / 255.0
+        let greenValue = Double((rgb >> 8) & 0xFF) / 255.0
+        let blueValue = Double(rgb & 0xFF) / 255.0
+        self.init(red: redValue, green: greenValue, blue: blueValue)
     }
 }
